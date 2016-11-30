97d27af5f9bf0947a2e0703c91f2d5c505ccf60d
Harrison Booth <harrisonbooth@ymail.com>
2016-11-30 15:36:33 +0000

    commit

--- player.rb
+++ player.rb
@@ -6,34 +6,32 @@ class Player
   attr_accessor :current_space
 
 #--------------Setup-----------------
-def initialize(name)
-  @name = name
-  @current_space = 0
-  
-end
+  def initialize(name)
+    @name = name
+    @current_space = 0
+    
+  end
 #------------------------------------
 
-def move(board)
-  win_space = board.spaces.index(board.spaces.last())
-  roll = rand(1..6)
-  puts "#{@name} rolled a #{roll}"
-  @current_space += roll
-  if @current_space > win_space
-    set(win_space)
+  def move(board)
+    win_space = board.spaces.index(board.spaces.last())
+    roll = rand(1..6)
+    puts "#{@name} rolled a #{roll}"
+    @current_space += roll
+    if @current_space > win_space
+      set(win_space)
+    end
   end
-end
 
-
-def set(destination)
-  @current_space = destination
-end
-
-def has_won?( board )
-  win_space = board.spaces.index(board.spaces.last())
-  if @current_space == win_space
-    return true
+  def set(destination)
+    @current_space = destination
   end
-end
 
+  def has_won?( board )
+    win_space = board.spaces.index(board.spaces.last())
+    if @current_space == win_space
+      return true
+    end
+  end
 
 end
