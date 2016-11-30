7065fa399b6e072c262aaffd723cf5c852d26b18
Harrison Booth <harrisonbooth@ymail.com>
2016-11-30 14:10:08 +0000

    Fix indentation, tidy win messages, remove un-needed warp messages

--- game.rb
+++ game.rb
@@ -10,31 +10,31 @@ class Game
     player_2 = Player.new("Harrison")
     
     # Create Spaces
-    space_0 = Space.new()
-    space_1 = Space.new()
-    space_2 = Space.new()
-    space_3 = Space.new()
-    space_4 = Space.new()
-    space_5 = Space.new()
-    space_6 = Space.new()
-    space_7 = Space.new()
-    space_8 = Space.new()
-    space_9 = Space.new()
-    space_10 = Space.new()
-    space_11 = Space.new()
-    space_12 = Space.new()
-    space_13 = Space.new()
-    space_14 = Space.new()
-    space_15 = Space.new()
-    space_16 = Space.new()
-    space_17 = Space.new()
-    space_18 = Space.new()
-    space_19 = Space.new()
-    space_20 = Space.new()
+      space_0 = Space.new()
+      space_1 = Space.new()
+      space_2 = Space.new()
+      space_3 = Space.new()
+      space_4 = Space.new()
+      space_5 = Space.new()
+      space_6 = Space.new()
+      space_7 = Space.new()
+      space_8 = Space.new()
+      space_9 = Space.new()
+      space_10 = Space.new()
+      space_11 = Space.new()
+      space_12 = Space.new()
+      space_13 = Space.new()
+      space_14 = Space.new()
+      space_15 = Space.new()
+      space_16 = Space.new()
+      space_17 = Space.new()
+      space_18 = Space.new()
+      space_19 = Space.new()
+      space_20 = Space.new()
 
     # Add Spaces to board
-    board.add_space_normal(space_0)
-    board.add_space_normal(space_1)
+      board.add_space_normal(space_0)
+      board.add_space_normal(space_1)
       board.add_space_special(space_2, 8) #ladder
       board.add_space_normal(space_3)
       board.add_space_normal(space_4)
@@ -58,24 +58,29 @@ class Game
       board.add_player(player_1)
       board.add_player(player_2)
 
-      while board.winner() == nil
-        board.players.cycle do |player|
-          puts "Nobody has one, thus far."
-          player.move(board)
-          puts "#{player.name()} has moved to space #{player.current_space()}"
-          player.set(board.spaces[player.current_space].end_space)
-          puts "#{player.name()} has been warped to space #{player.current_space()}"
+    while board.winner() == nil
+      board.players.cycle do |player|
 
-          if player.has_won?(board) == true
-            puts "Someone has won..."
-            board.winner = player.name()
-            break
-          end
-          puts ""
+        player.move(board)
+        position_before_set = player.current_space()
+        puts "#{player.name()} has moved to space #{player.current_space()}"
+        player.set(board.spaces[player.current_space].end_space)
+        position_after_set = player.current_space()
+
+        if position_after_set != position_before_set
+          puts "#{player.name()} has been warped to space #{player.current_space()}"
         end
-        puts "Winner: #{board.winner()}"
+
+        if player.has_won?(board) == true
+          puts "Someone has won..."
+          board.winner = player.name()
+          break
+        end
+        puts ""
       end
+      puts "Winner: #{board.winner()}"
     end
+  end
   end
 
   game = Game.new()
